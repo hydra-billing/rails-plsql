23b299ae068b635a8b8829140e653120dd77f962
Nikita Shilnikov <ns@hydra-billing.com>
2025-12-09 16:52:22 +0100

    Remove redundant require

--- lib/rails-plsql.rb
+++ lib/rails-plsql.rb
@@ -1,11 +1,3 @@
-if RUBY_ENGINE == 'ruby'
-  begin
-    require 'oci8'
-  rescue LoadError
-    # no oci8
-  end
-end
-
 require 'active_record'
 require 'rails/engine'
 require 'active_record/plsql/engine'
