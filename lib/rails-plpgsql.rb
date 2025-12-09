876cec427f2cdc61d33433e572a89bc93d4495b5
Nikita Shilnikov <ns@hydra-billing.com>
2025-12-09 12:24:11 +0100

    Delay code loading until adapter is known

--- lib/rails-plpgsql.rb
+++
@@ -1,4 +0,0 @@
-require 'ruby-plpgsql'
-require 'rails/engine'
-require 'active_record/plxsql/compat'
-require 'active_record/plpgsql/engine'
--- lib/rails-plsql.rb
+++ lib/rails-plsql.rb
@@ -5,14 +5,7 @@ if RUBY_ENGINE == 'ruby'
     # no oci8
   end
 end
-require 'active_record'
 
-begin
-  require 'activerecord-oracle_enhanced-adapter'
-  require 'ruby-plsql'
-  require 'oracle/named_error'
-  require 'rails/engine'
-  require 'active_record/plsql/engine'
-rescue LoadError
-  require 'rails-plpgsql'
-end
+require 'active_record'
+require 'rails/engine'
+require 'active_record/plsql/engine'
